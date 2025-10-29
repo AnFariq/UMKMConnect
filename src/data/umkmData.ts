1bd184ff27844606844b3b66c81fe269ef761619
Fariq <zeroy443@gmail.com>
2025-10-29 13:53:16 +0700

    Update data umkm
    ini update lagi ya

--- src/data/umkmData.ts
+++ src/data/umkmData.ts
@@ -28,5 +28,40 @@ export const umkmList = [
     category: 'Kuliner',
     description: 'Toko Roti dengan berbagai varian rasa',
     position: [-7.305902996124747, 112.77724152436119] as LatLngTuple
+  },
+  {
+    id: 5,
+    name: 'Es Teler Creamy 81',
+    category: 'Kuliner',
+    description: 'Es Teler dengan rasa creamy yang lezat',
+    position: [-7.308822962952968, 112.77860744197632] as LatLngTuple
+  },
+  {
+    id: 6,
+    name: 'Pouw Es Degan & Es Teler',
+    category: 'Kuliner',
+    description: 'Mbuh',
+    position: [-7.31175889780804, 112.78546187714464] as LatLngTuple
+  },
+  {
+    id: 7,
+    name: 'Es Teller Jayandaru & Baso Daging Sapi Asli',
+    category: 'Kuliner',
+    description: 'Mbuh',
+    position: [-7.3152117492474495, 112.78414844294905] as LatLngTuple
+  },
+  {
+    id: 8,
+    name: 'Pangsit Mie Ayam Tuban',
+    category: 'Kuliner',
+    description: 'isi en',
+    position: [-7.3097473054063595, 112.77974870977013] as LatLngTuple
+  },
+  {
+    id: 9,
+    name: 'Mie Ayam Cakrawala',
+    category: 'Kuliner',
+    description: 'mie',
+    position: [-7.313655430883333, 112.78354570240965] as LatLngTuple
   }
 ]
